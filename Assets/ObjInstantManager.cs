0bfc6c19ed5d1f9fed53b5b6d1b28162cabd42f3
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-17 05:25:53 +0900

    debug codes

--- Assets/ObjInstantManager.cs
+++ Assets/ObjInstantManager.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class ObjInstantManager : MonoBehaviour
 {
-    public void ObjInstant()
+    public void ObjInstant(Info[] infoArray)
     {
-
+        // using Debug
+        for (int i = 0; i < infoArray.Length; i++)
+        {
+            
+            Debug.Log(infoArray[i].id + " Obj");
+            Debug.Log(infoArray[i].region_name);
+            foreach (int a in infoArray[i].point)
+            {
+                Debug.Log(a);
+            }
+            Debug.Log(""); // Add an empty line between each Info object
+        }
     }
 }
