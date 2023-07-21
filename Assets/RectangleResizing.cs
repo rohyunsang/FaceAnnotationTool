9e7971c10d476dab6449ed285d17511c53e6ea89
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-21 15:36:15 +0900

    Update RectangleResizing.cs

--- Assets/RectangleResizing.cs
+++ Assets/RectangleResizing.cs
@@ -4,55 +4,54 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class RectangleResizing : MonoBehaviour, IDragHandler, IEndDragHandler //IScrollHandler
+public class RectangleResizing : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
 {
-    //Drag 
-    private RectTransform rectTr;
+    private RectTransform rectTransform;
+    private Vector2 initialMousePosition;
+    private Vector2 initialSize;
 
-    //Mouse Scorll Zoom In/Out
-    private Vector3 minitialScale;
-    private float zoomSpeed = 0.1f;
-    private float maxZoom = 10.0f;
-
-
-    private float initialDistance;
+    private bool resizing;
+    private float resizeFactor = 0.1f; // Resizing speed control
 
     private void Start()
     {
-        rectTr = GetComponent<RectTransform>(); //스크립트 위치의 Rect Transform
-        minitialScale = transform.localScale;  //현재 Local Scale을 저장
-
+        rectTransform = GetComponent<RectTransform>();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        rectTr.anchoredPosition += eventData.delta;  //드래그 이벤트 함수 선언 및 등록
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (!resizing)
+            {
+                resizing = true;
+                initialMousePosition = Camera.main.ScreenToWorldPoint(eventData.position);
+                initialSize = rectTransform.sizeDelta;
+            }
+            ResizeRectangle(eventData);
+        }
+        else
+        {
+            resizing = false;
+            rectTransform.anchoredPosition += eventData.delta;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        // rectTr.localPosition = Vector3.zero;  //드래그가 끝났을 때 중심으로 이동
+        resizing = false;
     }
 
-    // OnScroll 나중에 필요하면 구현.
-    /*
-     public void OnScroll(PointerEventData eventData) //마우스 스크롤 이벤트 등록
+    private void ResizeRectangle(PointerEventData eventData)
     {
-        var delta = Vector3.one * (eventData.scrollDelta.y * zoomSpeed);
-        var desiredScale = transform.localScale + delta;
+        Vector2 delta = (eventData.position - initialMousePosition) * resizeFactor;
+        Vector2 newSize = initialSize + delta;
 
-        desiredScale = ClampDesiredScale(desiredScale);
-
-        transform.localScale = desiredScale;
+        rectTransform.sizeDelta = newSize;
     }
-     */
 
-
-    private Vector3 ClampDesiredScale(Vector3 desiredScale) // 마우스 스크롤의 최대 줌인/아웃
+    public void OnPointerDown(PointerEventData eventData)
     {
-        desiredScale = Vector3.Max(minitialScale, desiredScale);
-        desiredScale = Vector3.Min(minitialScale * maxZoom, desiredScale);
-
-        return desiredScale;
+        // Unused for now but you might want to put something here in the future
     }
 }
