afc91b049a2d90f39cf4de0391355e6b7f20583a
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-07-30 16:57:36 +0900

    QueueManager 구현

--- Assets/JsonParsing.cs
+++ Assets/JsonParsing.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [System.Serializable]
@@ -20,15 +22,57 @@ public class Info  //structure
 
 public class JsonParsing : MonoBehaviour
 {
+    public GameObject PanelManagerObj;
     public GameObject ObjInstantGameObject; // using call ObjInstantManager Class Function
+    public RawImage faceImage;
+
+    public GameObject WorkEndImage;
+
     public List<string> jsonDatas = new List<string>();
+    public List<Texture2D> imageDatas = new List<Texture2D>();
     [SerializeField]
     private Info[] infoArray = new Info[8];
-
+    int idx = 0;
     public void MakeJsonArray(string jsonData)
     {
         jsonDatas.Add(jsonData);
     }
+    public void MakeImageStringArray(byte[] bytes)
+    {
+        // Create a Texture2D from the image bytes
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(bytes);
+        imageDatas.Add(texture);
+    }
+
+    public void QueueManager() // using btn;
+    {
+        if (jsonDatas.Count != imageDatas.Count)
+            Debug.Log("이미지와 데이터의 개수가 맞지 않습니다.");
+
+        if(jsonDatas.Count == idx)
+        {
+            WorkEndImage.SetActive(true);
+            jsonDatas.Clear();
+            imageDatas.Clear();
+            idx = 0;
+            StartCoroutine(InitPanelWithDelay(2f)); // start a coroutine to call OnInitPanel() after 2 seconds
+        }
+        else
+        {
+            ParseJSONData(jsonDatas[idx]);
+            faceImage.texture = imageDatas[idx];
+            idx++;
+        }
+    }
+    private IEnumerator InitPanelWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // wait for delay seconds
+        PanelManagerObj.GetComponent<PanelManager>().OnInitPanel();
+        WorkEndImage.SetActive(false);
+    }
+
+
     public void ParseJSONData(string jsonData)
     {
         // Deserialize the JSON data into the JSONData object
