4a25a101de80bef4031960aa416719b17f5b21ff
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-08-02 19:26:46 +0900

    Create Portrait.cs

---
+++ Assets/Portrait.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Portrait : MonoBehaviour
+{
+    public GameObject JsonParsingManagerObj;
+    public GameObject checkingImage;
+    public void PortraitClick()
+    {
+        JsonParsingManagerObj = GameObject.Find("JsonParsingManager");
+        JsonParsingManagerObj.GetComponent<JsonParsing>().QueueManager(int.Parse(transform.name));
+        
+    }
+    // need data delete json manager . parsing 한것으로 
+
+
+}
