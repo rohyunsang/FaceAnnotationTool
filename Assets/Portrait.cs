c93434733778b4e5e63e3ba9b86aad772e33af3e
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-08-15 05:52:28 +0900

    Update Portrait.cs

--- Assets/Portrait.cs
+++ Assets/Portrait.cs
@@ -8,10 +8,34 @@ public class Portrait : MonoBehaviour
 {
     public GameObject JsonParsingManagerObj;
     public GameObject checkingImage;
+
     public void PortraitClick()
     {
         JsonParsingManagerObj = GameObject.Find("JsonParsingManager");
-        JsonParsingManagerObj.GetComponent<JsonParsing>().QueueManager(int.Parse(transform.name));
+
+        string currentId = transform.name; // 여기서 transform.name이 해당 포트레이트의 id 값과 일치한다고 가정합니다.
+
+        int idx = GetIndexById(currentId);
+
+        if (idx != -1)  // 유효한 인덱스인 경우
+        {
+            Debug.Log(idx);
+            JsonParsingManagerObj.GetComponent<JsonParsing>().QueueManager(idx);
+        }
     }
-    // need data delete json manager . parsing 한것으로 
+
+    private int GetIndexById(string id)
+    {
+        List<Info> parsedInfo = JsonParsingManagerObj.GetComponent<JsonParsing>().parsedInfo;
+
+        for (int i = 0; i < parsedInfo.Count; i++)
+        {
+            if (parsedInfo[i].id == id)
+            {
+                return i;  // 인덱스를 반환
+            }
+        }
+
+        return -1;  // id를 찾지 못한 경우
+    }
 }
