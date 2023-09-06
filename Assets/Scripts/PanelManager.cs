1e128d4ec30b46fa5477c436036f013bc9c747bf
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-09-07 01:53:46 +0900

    face boundry line 추가

---
+++ Assets/Scripts/PanelManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelManager : MonoBehaviour  // panel and info controller
+{
+    public GameObject InitPanel;
+    public GameObject OptionPanel;
+    public GameObject SavePanel;
+    public GameObject LoginPanel;
+
+
+    public GameObject faceField;
+
+    public GameObject ImageClickInfo;
+    public GameObject ImageEditBtn;
+
+    public GameObject selectModeInfo;
+
+    public GameObject savejsonBtn;
+    public GameObject jsonExportBtn;
+    public GameObject saveFaceLineBtn;
+    public GameObject completeCountText;
+
+    public void OnClickBBOXModeBtn()
+    {
+        completeCountText.SetActive(true);
+        savejsonBtn.SetActive(true);
+        jsonExportBtn.SetActive(true);
+        saveFaceLineBtn.SetActive(false);
+    }
+    
+    public void OnClickFaceLineBtn()
+    {
+        completeCountText.SetActive(false);
+        savejsonBtn.SetActive(false);
+        jsonExportBtn.SetActive(false);
+        saveFaceLineBtn.SetActive(true);
+    }
+
+    public void OnSelectModeInfo()
+    {
+        selectModeInfo.SetActive(true);
+    }
+    public void OffSelectModeInfo()
+    {
+        selectModeInfo.SetActive(false);
+    }
+
+    public void OnImageEditBtn()
+    {
+        ImageEditBtn.SetActive(true);
+    }
+    public void OffImageEditBtn()
+    {
+        ImageEditBtn.SetActive(false);
+    }
+
+    public void OffImageClickInfo()
+    {
+        Invoke("InvokeOffImageClickInfo", 3f);
+    }
+
+    public void InvokeOffImageClickInfo()
+    {
+        ImageClickInfo.SetActive(false);
+    }
+    public void OnInitPanel()
+    {
+        InitPanel.SetActive(true);
+    }
+
+    public void OnOptionPanel()
+    {
+        OptionPanel.SetActive(true);
+    }
+    public void OnSavePanel()
+    {
+        SavePanel.SetActive(true);
+    }
+
+    public void OnLoginPanel()
+    {
+        LoginPanel.SetActive(true);
+    }
+    public void OffLoginPanel()
+    {
+        LoginPanel.SetActive(false);
+    }
+    public void OffSavePanel()
+    {
+        SavePanel.SetActive(false);
+    }
+
+    public void OffInitPanel()
+    {
+        InitPanel.SetActive(false);
+    }
+
+    public void OffOptionPanel()
+    {
+        OptionPanel.SetActive(false);
+    }
+    
+    public void DeleteObjects()
+    {
+        foreach (Transform child in faceField.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+}
