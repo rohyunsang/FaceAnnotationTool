5ff09c8bf95bcbcce2efa16e96bbf0f0382009ed
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-09-08 12:28:18 +0900

    Update ObjInstantManager.cs

--- Assets/Scripts/ObjInstantManager.cs
+++ Assets/Scripts/ObjInstantManager.cs
@@ -15,48 +15,7 @@ public class ObjInstantManager : MonoBehaviour
     private float PIXEL_FACEIMAGE_WIDTH = 715f;
     private const float PIXEL_FACEIMAGE_HEIGHT = 1080f;
 
-    public void ObjCircleInstant(List<Info> parsedInfo)
-    {
-        PIXEL_FACEIMAGE_WIDTH = PIXEL_WIDTH / PIXEL_HEIGHT * PIXEL_FACEIMAGE_HEIGHT;
-        Debug.Log(PIXEL_FACEIMAGE_WIDTH);
-        foreach (Info info in parsedInfo)
-        {
-            GameObjectList circleGameObjectList = new GameObjectList();
-            List<GameObject> newCircles = new List<GameObject>();
-            // faceLinePoints에서 각 원의 중심 좌표를 가져와 원 생성
-            for (int i = 0; i < info.faceLinePoints.Count; i += 2)
-            {
-                if (i + 1 < info.faceLinePoints.Count) // 좌표가 x, y 쌍으로 제공되므로 검사
-                {
-                    float centerX = info.faceLinePoints[i];
-                    float centerY = info.faceLinePoints[i + 1];
-                    
-
-                    // 원 객체 생성
-                    GameObject circle = Instantiate(circlePrefab, SpawnPoint.transform);
-
-                    // 원의 부모 설정
-                    circle.transform.SetParent(SpawnPoint.transform, false);
-
-                    // 원의 위치 설정
-                    RectTransform circleTransform = circle.GetComponent<RectTransform>();
-                    Vector2 circleCenter = new Vector2(centerX / PIXEL_WIDTH * PIXEL_FACEIMAGE_WIDTH, (PIXEL_HEIGHT - centerY) / PIXEL_WIDTH * PIXEL_FACEIMAGE_WIDTH);
-                    
-                    circleTransform.anchoredPosition = circleCenter - new Vector2(PIXEL_FACEIMAGE_WIDTH / 2, PIXEL_FACEIMAGE_HEIGHT / 2);
-                   
-                    // 원의 pivot 설정
-                    circleTransform.pivot = new Vector2(0.5f, 0.5f);
-                    circle.name = "face_line_" + (i / 2).ToString();
-                    // 생성된 원을 newCircles 리스트에 추가
-                    newCircles.Add(circle);
-                }
-            }
-
-            circleGameObjectList.gameObjects = newCircles;
-            JsonParsingObj.GetComponent<JsonParsing>().jsonCircles.Add(circleGameObjectList);
-        }
-
-    }
+    
     public void ObjRectangleInstant(List<Info> parsedInfo)
     {
         
@@ -72,6 +31,14 @@ public class ObjInstantManager : MonoBehaviour
                 int y1 = info.point[i*4 +1];
                 int x2 = info.point[i*4 +2];
                 int y2 = info.point[i*4 +3];
+                if(Mathf.Abs(x1 - x2) < 10)
+                {
+                    x2 = x1 + 18;
+                }
+                if(Mathf.Abs(y1 - y2) < 10)
+                {
+                    y2 = y1 + 18;
+                }
 
                 // Instantiate a rectangle object from the prefab
                 GameObject rectangle = Instantiate(rectanglePrefab, SpawnPoint.transform);
